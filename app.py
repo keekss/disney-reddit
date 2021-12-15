f904bbf3a20721e640c319e27dfeeba713cf0b1b
Keeks <65276627+keekss@users.noreply.github.com>
2021-12-15 06:11:26 -1000

    Plotly redo structure

--- app.py
+++ app.py
@@ -1,48 +1,91 @@
-# Run with terminal command 'python3 app.py'
+# Run with terminal command 'python3 app.py' from main project directory
 # View at http://127.0.0.1:8050/ in your web browser
 
-# Note: you may need to install additional libraries
-# if using a virtual environment, e.g.:
+# Note: you may need to install additional libraries, e.g.:
 ## dash
 ## dash_bootstrap_components
 ## dash_daq
+
+import pickle
+
+f = open('graphs.txt', 'rb')
+t = pickle.load(f)
 
 import pandas as pd
 from pandas.core.base import SelectionMixin
 
 import numpy as np
 
+# Import Dash and extra Plotly modules
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_daq as daq
 import dash_table as dt
 from dash_table.Format import Format, Scheme, Group
-import dash_bootstrap_components as dbc
-from plotly.express import data
-import dash_daq as daq
 from dash.dependencies import Input, Output, State
-
 from dash_bootstrap_templates import load_figure_template
 
 import plotly.express as px
+from plotly.express import data
+
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
+
+disney_dark_blue_hex = '#10194A'
+
+def graph_tab(name, number):
+    return dcc.Tab(
+        label = name,
+        value = 'tab-' + str(number),
+        className = 'graph-tab',
+    ),
+
+graph_file_names = [
+    'ticker_plot_dis.png',
+    'subreddit_post_scores.png',
+    'linear_regressions.png'
+]
 
 app.layout = dbc.Container(fluid = True, children = [
     dbc.Container(fluid = True, children = [
         dbc.Row([
             dbc.Col(
                 html.Section([
+                    html.H1(t['a']),
                     html.H1(
                         'Disney Subreddit Visualizations',
                         id = 'main-title',
                     ),
-                    html.H4(
-                        'ICS 484 - Final Project, Fall 2021 - Robert Lemon, Clark Whitehead, Kiko Whiteley',
-                        style = {'padding-top': '0px'}
+                    html.H5(
+                        'ICS 438 - Final Project, Fall 2021 - Andy Yu, Clark Whitehead, Frederick Straub, Kiko Whiteley',
+                        style = dict(paddingTop = '0px')
                     ),
+                    dbc.Row([
+                        dbc.Col(html.H5('Visit our:'), width = 3),
+                        dbc.Col(html.H5(
+                            dcc.Link(
+                                'GitHub Repository',
+                                href='https://github.com/keekss/disney-reddit',
+                                style = dict(color = disney_dark_blue_hex),
+                                target = "_blank"
+                            ), style = dict(color = disney_dark_blue_hex),
+                        ), width = 4),
+                        dbc.Col(html.H5(
+                            dcc.Link(
+                                'Project Writeup',
+                                href='https://github.com/keekss/disney-reddit/blob/master/438%20Final%20Report.ipynb',
+                                style = dict(color = disney_dark_blue_hex),
+                                target = "_blank"
+                            ), style = dict(color = disney_dark_blue_hex),
+                        ), width = 5),
+                    ], style = dict(paddingTop = '10px')),
+                    html.H4(
+                       
+                    ),                    
                 ]),
                 width = 6
             ),
@@ -56,14 +99,19 @@ app.layout = dbc.Container(fluid = True, children = [
                                 dcc.Dropdown(
                                     id = 'extra-info-dropdown',
                                     options =[
-                                        dict(label = 'Reddit Sentiment & Stock API', value = 'api'),
+                                        dict(label = 'Project Writeup', value = 'writeup'),
                                         dict(label = 'PMAW', value = 'pmaw'),
                                         dict(label = 'Pushshift', value = 'pushshift'),
                                         dict(label = 'yfinance', value = 'yfinance'),
                                     ],
+                                    searchable = True,
                                 ),
-                            ])], width = 3),
-                        dbc.Col(html.H5(id = 'extra-info-text'), width = 9),
+                            ])], width = 4),
+                        dbc.Col(
+                            html.H4(id = 'extra-info-text'), 
+                            width = 8,
+                            style = dict(margin = 'auto')
+                        ),
                     ]),
                 ])
             )
@@ -72,19 +120,19 @@ app.layout = dbc.Container(fluid = True, children = [
     dbc.Container(fluid = True, children = [
         dcc.Tabs(
         id = 'tabs',
-        colors = {
-            'primary': 'black',
-            'background': '#393F8F',
-            'border': 'whitesmoke'
-        },
+        colors = dict(
+            primary = 'black',
+            background = '#393F8F',
+            border = 'whitesmoke'
+        ),
         children = [
             dcc.Tab(
-                label = '1: Character Mentions',
+                label = '1: Disney Stock Prices',
                 value = 'tab-1',
                 className = 'graph-tab',
             ),
             dcc.Tab(
-                label = '2: Disneyland Ride Sentiments',
+                label = '2: Subreddit Post Scores',
                 value = 'tab-2',
                 className = 'graph-tab',
 
@@ -111,10 +159,28 @@ graph_font = dict(
     size = 24,
 )
 
-# Tab 1: Character Mentions
-tab_1 = dbc.Row([
-    html.Img(src=app.get_asset_url('charactersChart.png'))
-])
+def graph_container(image_file_path):
+    return dbc.Row(
+        html.Img(
+            src=app.get_asset_url(image_file_path),
+            className = 'graph'
+        ),
+        className = 'graph-container'
+    )
+
+tab_1 = graph_container('ticker_plot_dis.png')
+
+tab_2 = graph_container('subreddit_post_scores.png')
+
+tab_3 = graph_container('linear_regressions.png')
+
+tab_2 = dbc.Row(
+    html.Img(
+        src=app.get_asset_url('ticker_plot_dis.png'),
+        className = 'graph'
+    ),
+    className = 'graph-container'
+)
 
 # Tab 2: Disneyland Ride Sentiments
 
@@ -148,13 +214,16 @@ colormap = cm.LinearColormap(
 
 #iterate through the dataframe and add each attraction to the feature group
 for index, row in data.iterrows():
-    fg.add_child(folium.Marker(location=[row["lat"], row["long"]], popup="{} : {}".format(row["ride"].upper(), round(row['sentiment'], 2)), icon=folium.Icon(color="black", icon_color=colormap(row['sentiment']))))
+    fg.add_child(folium.Marker(
+        location=[row["lat"], row["long"]], 
+        popup="{} : {}".format(row["ride"].upper(), round(row['sentiment'], 2)), 
+        icon=folium.Icon(color="black", icon_color=colormap(row['sentiment'])))
+    )
 
 map.add_child(colormap)
     
 #add the feature group to the map
 map.add_child(fg)
-
 
 ride_map = map.save('ride_map.html')
 
@@ -283,18 +352,42 @@ def render_content(tab):
         return tab_3b
 
 extra_info_dict = dict(
-    api = [
-        'Our group authored an API to allow users to fetch Reddit post sentiment and stock price data for analysis for a given subreddit and associated stock.',
-        html.Br(), html.Br(),
+    writeup = [
+        'All data processing and methodologies are detailed in a Jupyter Notebook',
+        html.Br(),
+        'Visualizations are included in-line.',
+        html.Br(),
         dcc.Link(
-            'Click here to view the Jupyter Notebook',
-            href='https://github.com/keekss/ics-484-final/blob/main/Reddit_Sentiment_vs_Stock_API.ipynb',
-            style = dict(color = '#10194A')
+            'View the Project Writeup',
+            href='https://github.com/keekss/disney-reddit/blob/master/438%20Final%20Report.ipynb',
+            style = dict(color = disney_dark_blue_hex),
         )
     ],
-    pmaw = '',
-    pushshift = '',
-    yfinance = ''
+    pmaw = [
+        'PMAW (Pushshift Multithread API Wrapper) uses the Pushshift API (see \"Pushshift\" info) to multithread ineractions of Pushshift endpoints to improve performance.  ',
+        dcc.Link(
+            'View the GitHub Repository',
+            href='https://github.com/mattpodolak/pmaw',
+            style = dict(color = disney_dark_blue_hex),
+        )
+    ],
+    pushshift = [
+        'Pushshift allows users to search Reddit.com posts and comments based on various criteria, such as keywords, creation date, and score.  ',
+        html.Br(),
+        dcc.Link(
+            'View the GitHub Repository',
+            href='https://github.com/pushshift/api',
+            style = dict(color = disney_dark_blue_hex),
+        )
+    ],
+    yfinance = [
+        'yfinance allows users to access market data from Yahoo Finance, one of the leading financial media services.  Our group used yfinance to pull historical stock price data.  ',
+        dcc.Link(
+            'View the PyPI Project Page',
+            href='https://pypi.org/project/yfinance/',
+            style = dict(color = disney_dark_blue_hex),
+        )
+    ],
 )
 
 @app.callback(
